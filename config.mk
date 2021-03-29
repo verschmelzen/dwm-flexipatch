356b36cee8fc235d2120da1886b3929c28dcfa35
verschmelzen <ivan.komarov@protonmail.com>
2021-03-29 20:36:10 +0300

    Select patches, make config, edit config.mk

--- config.mk
+++ config.mk
@@ -39,14 +39,14 @@ FREETYPEINC = /usr/include/freetype2
 #PANGOLIB = `pkg-config --libs xft pango pangoxft`
 
 # Uncomment for the ipc patch / IPC_PATCH
-#YAJLLIBS = -lyajl
-#YAJLINC = -I/usr/include/yajl
+# YAJLLIBS = -lyajl
+# YAJLINC = -I/usr/include/yajl
 
 # Uncomment this for the rounded corners patch / ROUNDED_CORNERS_PATCH
 #XEXTLIB = -lXext
 
 # Uncomment this for the swallow patch / SWALLOW_PATCH
-#XCBLIBS = -lX11-xcb -lxcb -lxcb-res
+XCBLIBS = -lX11-xcb -lxcb -lxcb-res
 
 # includes and libs
 INCS = -I${X11INC} -I${FREETYPEINC} ${YAJLINC} ${PANGOINC}
